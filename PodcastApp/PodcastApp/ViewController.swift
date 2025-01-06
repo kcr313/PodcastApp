674b667a4381e1f52e1ede3fbe365caa62ec48e2
Kadir ecer <98332402+kcr313@users.noreply.github.com>
2025-01-06 19:59:57 +0300

    gitignore updated

---
+++ PodcastApp/PodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by kadir ecer on 6.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
